9f644519909ddf1842e896b3d28e7485399d65d6
AMS006 <anassain13@gmail.com>
2023-03-30 14:29:56 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(cors({
     credentials: true
 }))
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://localhost:4200");
+    res.header('Access-Control-Allow-Origin', "https://m-video-client.vercel.app");
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');

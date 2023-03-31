92b9d0ecc6aa147e33c08dc66097d3ccbd6b34df
AMS006 <anassain13@gmail.com>
2023-03-31 17:14:02 +0530

    Updated

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -54,9 +54,9 @@ exports.loginUser = async(req,res) =>{
 exports.logoutUser = async(req,res) =>{
     try {
         const option = {
-            expires: new Date(Date.now()),
+            expires: new Date(Date.now())
         }
-        return res.cookie('token',null,option).json({message:"Logout Successfull"})
+        res.cookie('token',null,option).json({message:"Logout Successfull"})
     } catch (error) {
         return res.status(500).json({message:error.message});
     }
